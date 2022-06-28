21d6bb17cba183687a6d72669eeeb24c7998440f
Ambagasdowa <baizabal.jesus@gmail.com>
2022-06-28 10:09:05 -0500

    Trying Computer issue v0.06

--- ms_geolocation/class/geolocation.class.php
+++ ms_geolocation/class/geolocation.class.php
@@ -256,7 +256,8 @@ public function ajaxtab_entete_fixe_($columns, $default_fields, $option = array(
     $opt = false;
     ?>
 
-<?php $this->debug($_SERVER); ?>
+<?php $this->debug($_GET); ?>
+<?php $this->debug($_POST); ?>
 
     <div align=center>
         <div class="<?php echo $option['table_name']; ?>_top_settings" style="display:none;">
